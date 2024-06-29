ac7de356d8d90315ca14e327a33c19fa0492ac90
kamazitun.jaykehd@gmail.com <kamazitun.jaykehd@gmail.com>
2024-06-29 23:44:10 +0300

    added command base

--- CandyKeeper.Presentaion/ViewModels/MainWindowsViewModel.cs
+++ CandyKeeper.Presentaion/ViewModels/MainWindowsViewModel.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using CandyKeeper.Presentaion.ViewModels.Base;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Markup;
 
 namespace CandyKeeper.Presentaion.ViewModels
 {
-    internal class MainWindowsViewModel
+    internal class MainWindowsViewModel : ViewModel
     {
+        private string _title = "CandyKeeper";
+
+        public string Title
+        {
+            get => _title;
+            set => Set(ref _title, value);
+        } 
     }
 }
