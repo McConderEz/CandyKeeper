3d42dcd98aa6f24d4e56ba6e9b737ba14630993f
kamazitun.jaykehd@gmail.com <kamazitun.jaykehd@gmail.com>
2024-06-30 15:57:08 +0300

    dev main window

--- CandyKeeper.Presentaion/ViewModels/MainWindowsViewModel.cs
+++ CandyKeeper.Presentaion/ViewModels/MainWindowsViewModel.cs
@@ -1,9 +1,12 @@
-﻿using CandyKeeper.Presentaion.ViewModels.Base;
+﻿using CandyKeeper.Presentaion.Infrastructure.Commands;
+using CandyKeeper.Presentaion.ViewModels.Base;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 using System.Windows.Markup;
 
 namespace CandyKeeper.Presentaion.ViewModels
@@ -11,11 +14,33 @@ namespace CandyKeeper.Presentaion.ViewModels
     internal class MainWindowsViewModel : ViewModel
     {
         private string _title = "CandyKeeper";
-
         public string Title
         {
             get => _title;
             set => Set(ref _title, value);
-        } 
+        }
+
+        #region Команды
+
+        #region CloseApplicationCommand
+        public ICommand CloseApplicationCommand { get; }
+
+        private void OnCloseApplicationCommandExecute(object p)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private bool CanCloseApplicationCommandExecute(object p) => true;
+        #endregion
+
+        #endregion
+
+        public MainWindowsViewModel()
+        {
+            #region Команды
+            CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecute);
+
+            #endregion
+        }
     }
 }
