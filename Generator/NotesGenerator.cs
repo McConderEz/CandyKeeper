e5919276e5bc982bbd11965cfe2ff5c11cb1f8f6
kamazitun.jaykehd@gmail.com <kamazitun.jaykehd@gmail.com>
2024-07-02 00:29:21 +0300

    generator: added gen all tables

--- Generator/NotesGenerator.cs
+++ Generator/NotesGenerator.cs
@@ -24,7 +24,12 @@ namespace Generator
             //GenDistrict();
             //GenOwnershipType();
             //GenProductType();
-            GenPackaging();
+            //GenPackaging();
+            //GenProduct();
+            //GenSupplier();
+            //GenStore();
+            //GenProductDelivery();
+            GenProductForSale();
         }
 
 
@@ -85,6 +90,150 @@ namespace Generator
             _dbContext.SaveChanges();
         }
 
+
+        public void GenProduct()
+        {
+            var list = _parser.ParseCSV(PATH + "Product.csv");
+
+            for (var i = 0; i < list.Count; i++)
+            {
+                var productType = _dbContext.ProductTypes.First(c => c.Name.Equals(list[i].Item2.First()));
+                _dbContext.Products.Add(new ProductEntity { Name = list[i].Item1, ProductTypeId = productType.Id });
+            }
+            _dbContext.SaveChanges();
+        }
+
+        public static string GeneratePhoneNumber()
+        {
+            Random random = new Random();
+
+            int countryCodeLength = random.Next(1, 4);
+            string countryCode = GenerateRandomDigits(countryCodeLength, random);
+
+            string mainNumber = GenerateRandomDigits(10, random);
+
+            string phoneNumber = $"+{countryCode}{mainNumber}";
+
+            return phoneNumber;
+        }
+
+        private static string GenerateRandomDigits(int length, Random random)
+        {
+            char[] digits = new char[length];
+            for (int i = 0; i < length; i++)
+            {
+                digits[i] = (char)('0' + random.Next(0, 10));
+            }
+
+            return new string(digits);
+        }
+
+        public static DateTime GenerateRandomDate(DateTime startDate, DateTime endDate)
+        {
+            Random random = new Random();
+            int range = (endDate - startDate).Days;
+            return startDate.AddDays(random.Next(range));
+        }
+
+        public void GenSupplier()
+        {
+            var list = _parser.Parse(PATH + "SuppliersName.txt");
+            var cityIds = _dbContext.Cities.Select(c => c.Id).ToList();
+            var ownershipTypeIds = _dbContext.OwnershipTypes.Select(o => o.Id).ToList();
+            DateTime startDate = new DateTime(1930, 1, 1);
+            DateTime endDate = DateTime.Now;
+
+            for (var i = 0; i < list.Count; i++)
+            {
+                for(var j = 0; j < 5; j++)
+                {
+                    _dbContext.Suppliers.Add(new SupplierEntity
+                    {
+                        Name = list[i],
+                        CityId = cityIds[new Random().Next(0, cityIds.Count - 1)],
+                        OwnershipTypeId = ownershipTypeIds[new Random().Next(0, ownershipTypeIds.Count - 1)],
+                        Phone = GeneratePhoneNumber()
+                    });
+                }
+            }
+            _dbContext.SaveChanges();
+        }
+
+        public void GenStore()
+        {
+            var list = _parser.Parse(PATH + "SuppliersName.txt");
+            var districtIds = _dbContext.Districts.Select(d => d.Id).ToList();
+            var ownershipTypeIds = _dbContext.OwnershipTypes.Select(o => o.Id).ToList();
+            DateTime startDate = new DateTime(1930, 1, 1);
+            DateTime endDate = DateTime.Now;
+
+            for (var i = 0; i < list.Count; i++)
+            {
+                for (var j = 0; j < 10; j++)
+                {
+                    _dbContext.Stores.Add(new StoreEntity
+                    {
+                        Name = list[i],
+                        StoreNumber = new Random().Next(100000,1000000),
+                        DistrictId = districtIds[new Random().Next(0, districtIds.Count - 1)],
+                        OwnershipTypeId = ownershipTypeIds[new Random().Next(0, ownershipTypeIds.Count - 1)],
+                        Phone = GeneratePhoneNumber(),
+                        YearOfOpened = GenerateRandomDate(startDate, endDate)
+                    });
+                }
+            }
+            _dbContext.SaveChanges();
+        }
+
+        public void GenProductDelivery()
+        {
+            var supplierIds = _dbContext.Suppliers.Select(s => s.Id).ToList();
+            var storeIds = _dbContext.Stores.Select(s => s.Id).ToList();
+            DateTime startDate = new DateTime(2015, 1, 1);
+            DateTime endDate = DateTime.Now;
+
+            for (var i = 0; i < storeIds.Count; i++)
+            {
+                for (var j = 0; j < 5; j++)
+                {
+                    _dbContext.ProductDeliveries.Add(new ProductDeliveryEntity
+                    {
+                        DeliveryDate = GenerateRandomDate(startDate, endDate),
+                        SupplierId = supplierIds[new Random().Next(0, supplierIds.Count - 1)],
+                        StoreId = storeIds[i]
+                    });
+                }
+            }
+            _dbContext.SaveChanges();
+        }
+
+
+        public void GenProductForSale()
+        {
+            var deliveries = _dbContext.ProductDeliveries.ToList();
+            var storeIds = _dbContext.Stores.Select(s => s.Id).ToList();
+            var productIds = _dbContext.Products.Select(p => p.Id).ToList();
+            var packagingIds = _dbContext.Packaging.Select(p => p.Id).ToList();
+            DateTime startDate = new DateTime(2015, 1, 1);
+            DateTime endDate = DateTime.Now;
+
+            for (var i = 0; i < deliveries.Count; i++)
+            {
+                for (var j = 0; j < 5; j++)
+                {
+                    _dbContext.ProductForSales.Add(new ProductForSaleEntity
+                    {
+                        ProductId = productIds[new Random().Next(0, productIds.Count - 1)],
+                        StoreId = deliveries[i].StoreId,
+                        ProductDeliveryId = deliveries[i].Id,
+                        PackagingId = packagingIds[new Random().Next(0, packagingIds.Count - 1)],
+                        Price = new Random().Next(10, 1000000),
+                        Volume = new Random().Next(1, 100000)
+                    });
+                }
+            }
+            _dbContext.SaveChanges();
+        }
     }
     
 }
