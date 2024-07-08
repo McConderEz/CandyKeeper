83eabca764bf9020087ad5a7f721f8ea37a4813e
kamazitun.jaykehd@gmail.com <kamazitun.jaykehd@gmail.com>
2024-07-09 01:34:27 +0300

    refactoring solution

---
+++ Generator/Program.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using CandyKeeper.DAL;
+using CandyKeeper.DAL.Repositories;
+using CandyKeeper.Domain.Models;
+using Generator;
+using Microsoft.EntityFrameworkCore;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //var generator = new NotesGenerator();
+        //generator.GenAll();
+    }
+}
+
